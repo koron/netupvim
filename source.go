b5c1678f07eecee86ab54212a9e0d51d3d3582ef
MURAOKA Taro <koron.kaoriya@gmail.com>
2016-04-11 01:04:57 +0900

    github source

--- source.go
+++ source.go
@@ -12,12 +12,17 @@ import (
 	"time"
 
 	"github.com/koron/go-arch"
+	"github.com/koron/go-github"
 )
 
 var (
 	errUnknownSource     = errors.New("unknown source")
 	errSourceNotFound    = errors.New("source not found")
 	errSourceNotModified = errors.New("source not modified")
+
+	errGithubNoRelease       = errors.New("absence of github release")
+	errGithubNoAssets        = errors.New("no matched assets in github release")
+	errGithubIncompleteAsset = errors.New("incomplete github asset")
 )
 
 type sourceType int
@@ -41,6 +46,8 @@ func toSourceType(s string) (sourceType, error) {
 }
 
 type source interface {
+	// download downloads source file to outdir, return its path name.
+	// if pivot is not zero, this checks changes of source from pivot.
 	download(outdir string, pivot time.Time) (path string, err error)
 }
 
@@ -51,77 +58,63 @@ type directSource struct {
 var _ source = (*directSource)(nil)
 
 func (ds *directSource) download(d string, p time.Time) (string, error) {
-	out, err := ds.outPath(d)
-	if err != nil {
-		return "", err
-	}
-
-	req, err := http.NewRequest("GET", ds.url, nil)
-	if err != nil {
-		return "", err
-	}
-	if !p.IsZero() {
-		t := p.UTC().Format(http.TimeFormat)
-		req.Header.Set("If-Modified-Since", t)
-	}
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	switch resp.StatusCode {
-	case http.StatusOK:
-		f, err := os.Create(out)
-		if err != nil {
-			return "", err
-		}
-		defer f.Close()
-		if _, err := io.Copy(f, resp.Body); err != nil {
-			return "", err
-		}
-
-	case http.StatusNotModified:
-		return "", errSourceNotModified
-
-	default:
-		return "", fmt.Errorf("unexpected response: %s", resp.Status)
-	}
-	return out, nil
-}
-
-func (ds *directSource) outPath(d string) (string, error) {
-	u, err := url.Parse(ds.url)
-	if err != nil {
-		return "", err
-	}
-	return filepath.Join(d, filepath.Base(u.Path)), nil
+	return download(d, ds.url, p)
 }
 
 type githubSource struct {
-	user        string
-	project     string
-	namePattern *regexp.Regexp
+	user    string
+	project string
+	namePat *regexp.Regexp
 }
 
 var _ source = (*githubSource)(nil)
 
-func (gs *githubSource) download(d string, a time.Time) (string, error) {
-	// TODO:
-	return "", errors.New("not implmented yet")
+func (gs *githubSource) download(d string, p time.Time) (string, error) {
+	a, err := gs.fetchAsset()
+	if err != nil {
+		return "", err
+	}
+	if !p.IsZero() && p.After(a.UpdatedAt) {
+		return "", errSourceNotModified
+	}
+	return download(a.DownloadURL, d, p)
+}
+
+func (gs *githubSource) fetchAsset() (*github.Asset, error) {
+	r, err := github.Latest(gs.user, gs.project)
+	if err != nil {
+		return nil, err
+	}
+	if r.Draft || r.PreRelease {
+		return nil, errGithubNoRelease
+	}
+	var t *github.Asset
+	for _, a := range r.Assets {
+		if gs.namePat.MatchString(a.Name) {
+			t = &a
+			break
+		}
+	}
+	if t == nil {
+		return nil, errGithubNoAssets
+	}
+	if t.State != "uploaded" {
+		return nil, errGithubIncompleteAsset
+	}
+	return t, nil
 }
 
 var sources = map[sourceType]map[arch.CPU]source{
 	releaseSource: {
 		arch.X86: &githubSource{
-			user:        "koron",
-			project:     "vim-kaoriya",
-			namePattern: regexp.MustCompile(`-win32-.*\.zip$`),
+			user:    "koron",
+			project: "vim-kaoriya",
+			namePat: regexp.MustCompile(`-win32-.*\.zip$`),
 		},
 		arch.AMD64: &githubSource{
-			user:        "koron",
-			project:     "vim-kaoriya",
-			namePattern: regexp.MustCompile(`-win64-.*\.zip$`),
+			user:    "koron",
+			project: "vim-kaoriya",
+			namePat: regexp.MustCompile(`-win64-.*\.zip$`),
 		},
 	},
 	developSource: {
@@ -152,4 +145,58 @@ func determineSource(st sourceType, cpu arch.CPU) (source, error) {
 		return nil, errSourceNotFound
 	}
 	return s, nil
+}
+
+func downloadFilepath(inURL, outdir string) (string, error) {
+	u, err := url.Parse(inURL)
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(outdir, filepath.Base(u.Path)), nil
+}
+
+func downloadAsFile(inURL, outPath string, pivot time.Time) error {
+	req, err := http.NewRequest("GET", inURL, nil)
+	if err != nil {
+		return err
+	}
+	if !pivot.IsZero() {
+		t := pivot.UTC().Format(http.TimeFormat)
+		req.Header.Set("If-Modified-Since", t)
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	switch resp.StatusCode {
+	case http.StatusOK:
+		f, err := os.Create(outPath)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		if _, err := io.Copy(f, resp.Body); err != nil {
+			return err
+		}
+	case http.StatusNotModified:
+		return errSourceNotModified
+	default:
+		return fmt.Errorf("unexpected response: %s", resp.Status)
+	}
+	return nil
+}
+
+// download downloads URL and saves as a file to outdir, return its path name.
+// if pivot is not zero, this checks changes of source after pivot.
+func download(inURL, outdir string, pivot time.Time) (string, error) {
+	path, err := downloadFilepath(inURL, outdir)
+	if err != nil {
+		return "", err
+	}
+	if err := downloadAsFile(inURL, path, pivot); err != nil {
+		return "", err
+	}
+	return path, nil
 }
