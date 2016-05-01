0a828fded6e489c43c478b81cf4839485574ee6f
MURAOKA Taro <koron.kaoriya@gmail.com>
2016-05-01 21:35:56 +0900

    use formatter

--- config_test.go
+++ config_test.go
@@ -8,10 +8,10 @@ func TestLoadConfigEmpty(t *testing.T) {
 		t.Fatalf("loadConfig(not_exist) should be succeeded: %s", err)
 	}
 	if c.Source != "" {
-		t.Error("c.Source should be empty: %q", c.Source)
+		t.Errorf("c.Source should be empty: %q", c.Source)
 	}
 	if c.TargetDir != "" {
-		t.Error("c.TargetDir should be empty: %q", c.TargetDir)
+		t.Errorf("c.TargetDir should be empty: %q", c.TargetDir)
 	}
 }
 
@@ -21,10 +21,10 @@ func TestLoadConfigSource(t *testing.T) {
 		t.Fatalf("loadConfig(source_only) should be succeeded: %s", err)
 	}
 	if c.Source != "foo" {
-		t.Error("c.Source should be \"foo\": %q", c.Source)
+		t.Errorf("c.Source should be \"foo\": %q", c.Source)
 	}
 	if c.TargetDir != "" {
-		t.Error("c.TargetDir should be empty: %q", c.TargetDir)
+		t.Errorf("c.TargetDir should be empty: %q", c.TargetDir)
 	}
 }
 
@@ -34,10 +34,10 @@ func TestLoadConfigTargetDir(t *testing.T) {
 		t.Fatalf("loadConfig(target_dir_only) should be succeeded: %s", err)
 	}
 	if c.Source != "" {
-		t.Error("c.Source should be empty: %q", c.Source)
+		t.Errorf("c.Source should be empty: %q", c.Source)
 	}
 	if c.TargetDir != "bar" {
-		t.Error("c.TargetDir should be \"bar\": %q", c.TargetDir)
+		t.Errorf("c.TargetDir should be \"bar\": %q", c.TargetDir)
 	}
 }
 
@@ -47,9 +47,9 @@ func TestLoadConfigAll(t *testing.T) {
 		t.Fatalf("loadConfig(target_dir_only) should be succeeded: %s", err)
 	}
 	if c.Source != "foo" {
-		t.Error("c.Source should be \"foo\": %q", c.Source)
+		t.Errorf("c.Source should be \"foo\": %q", c.Source)
 	}
 	if c.TargetDir != "bar" {
-		t.Error("c.TargetDir should be \"bar\": %q", c.TargetDir)
+		t.Errorf("c.TargetDir should be \"bar\": %q", c.TargetDir)
 	}
 }
